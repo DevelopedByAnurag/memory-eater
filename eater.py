fcfb6583021bf0f7f1614331ebe4a9a5c14c16d8
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2019-01-19 16:23:47 +0530

    Create eater.py

---
+++ eater.py
@@ -0,0 +1,13 @@
+"""
+This Script Basically Crashes any PC Dramatically....
+Question:
+Why do any programmer want that?
+Answer:
+IDK, For fun i guess :).
+===============
+Written By:
+- Anurag batra
+===============
+"""
+
+l=[i for i in range(536870912)]
